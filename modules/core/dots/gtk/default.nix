d06e59b4ca42374e4dfffbca914f4a3bf950119c
desiirenav <desiirenav@hotmail.com>
2025-12-04 00:52:48 -0500

    gtk update

--- modules/core/dots/gtk/default.nix
+++ modules/core/dots/gtk/default.nix
@@ -2,25 +2,7 @@
   inherit (lib) mkDefault;
   theme-name = "adw-gtk3";
   icon-theme-name = "Adwaita";
-  gtk-css = ''
-	@define-color accent_color #${config.theme.base0D};
-	@define-color accent_fg_color #${config.theme.base0A};
-	@define-color accent_bg_color #${config.theme.base07};
-	@define-color window_bg_color #${config.theme.base00};
-	@define-color window_fg_color #${config.theme.base07};
-	@define-color headerbar_bg_color #${config.theme.base00};
-	@define-color headerbar_fg_color #${config.theme.base07};
-	@define-color popover_bg_color #${config.theme.base00};
-	@define-color popover_fg_color #${config.theme.base07};
-	@define-color view_bg_color #${config.theme.base00};
-	@define-color view_fg_color #${config.theme.base07};
-	@define-color card_bg_color #${config.theme.base00};
-	@define-color card_fg_color #${config.theme.base07};
-	@define-color sidebar_bg_color @window_bg_color;
-	@define-color sidebar_fg_color @window_fg_color;
-	@define-color sidebar_border_color @window_bg_color;
-	@define-color sidebar_backdrop_color @window_bg_color;
-  '';
+  gtk-css = ./gtk-css.nix {inherit config;};
 in {
   hjem.users.narayan.files = {
     ".config/gtk-3.0/settings.ini".text = ''
@@ -41,26 +23,25 @@ in {
     '';
   };
 
-    programs = {
-      dconf = {
-        enable = mkDefault true;
-        profiles = {
-          user = {
-            databases = [
-              {
-                lockAll = false;
-                settings = {
-                  "org/gnome/desktop/interface" = {
-                    gtk-theme = theme-name;
-                    icon-theme = icon-theme-name;
-                    color-scheme = "prefer-dark";
-                  };
-                };
-              }
-            ];
-          };
+  programs = {
+    dconf = {
+      enable = mkDefault true;
+      profiles = {
+        user = {
+          databases = [
+            {
+	      lockAll = false;
+	      settings = {
+	        "org/gnome/desktop/interface" = {
+	          gtk-theme = theme-name;
+	          icon-theme = icon-theme-name;
+	          color-scheme = "prefer-dark";
+	        };
+	      };
+            }
+          ];
         };
       };
     };
-
+  };
 } 
