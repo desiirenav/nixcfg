3181b8ce23b263c40409f86fe65ed6706a2264c6
desiirenav <desiirenav@gmail.com>
2025-12-20 14:20:14 -0500

    ignis wrapper with flags

--- modules/packages/ignis/default.nix
+++ modules/packages/ignis/default.nix
@@ -1,13 +1,14 @@
 { config, inputs, pkgs, ... }: {
   environment.systemPackages = [
     (inputs.wrappers.lib.wrapPackage {
-      package = (pkgs.inputs.ignis.packages.${pkgs.system}.default.override {
+      inherit pkgs;
+      package = (inputs.ignis.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
         enableAudioService = true;
 	useDartSass = true;
 	extraPackages = [];
       });
       flags = {
-        "init" = {};
+        "init" = "";
 	"run-file" = "/home/narayan/nixcfg/modules/packages/ignis/config";
       };
     })
