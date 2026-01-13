81db4359a91ca091c7a3e42bfae0c2fcd0e1e602
desiirenav <desiirenav@gmail.com>
2026-01-12 20:13:47 -0500

    redoing my quickshell config from scratch

--- modules/packages/niri/default.nix
+++ modules/packages/niri/default.nix
@@ -65,9 +65,8 @@
 
 	prefer-no-csd
 
-//	spawn-at-startup "xwayland-satellite"
 	spawn-sh-at-startup "swaybg -i /home/narayan/nixcfg/modules/packages/niri/lavender.png"
-	spawn-at-startup "quickshell"
+//	spawn-at-startup "quickshell"
 
 	screenshot-path "~/Pictures/Screenshots/Screenshot from %Y-%m-%d %H-%M-%S.png"
 
