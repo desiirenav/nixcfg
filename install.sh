e9a49da8e17a750456fb1e4f4cc51a1f7f0e9aee
desiirenav <desiirenav@hotmail.com>
2026-01-22 03:52:58 +0000

    Fix install script to update flake before installation

--- install.sh
+++ install.sh
@@ -54,6 +54,7 @@ git clone https://github.com/desiirenav/nixcfg.git && \
 cd /home/nixos/nixcfg && \
 sudo rm -rf .git && \
 NIX_CONFIG="experimental-features = nix-command flakes" \
+  nix flake update && \
   sudo nixos-install --flake .#nixos --no-root-passwd && \
 cd -
 sudo cp -r nixcfg/ /mnt/persist
